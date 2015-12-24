5fc446c573b63c9bb3b3ae973e039a55def79de0
matekro <winepastor@gmail.com>
2015-12-24 09:23:12 +0800

    init flask

---
+++ README.md
@@ -0,0 +1,5 @@
+1. 网站上面点fork，生成自己的副本
+2. 在自己的副本里修改，commit，push，这些修改只会提交到自己的repo(fork出来的那个
+3. 在网站上点new pull request，申请把修改拉回repo的原owner(rsl4
+4. 原owner点同意，merge修改
+5. github官方help有说明正确的协作流程
---
+++ index.py
@@ -0,0 +1,105 @@
+# all the imports
+import sys, os
+from sqlite3 import dbapi2 as sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from contextlib import closing
+
+
+#creat application:
+app = Flask(__name__)
+
+# load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE=os.path.join('./db/', 'flaskr.db'),
+    DEBUG=True,
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+
+# app.config.from_object(__name__)
+app.config.from_envvar('./db/FLASKR_SETTINGS', silent=True)
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('./db/schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+            db.commit()
+
+def get_db():
+    """
+    Opens a new database connection if there is none yet for the
+    current application context.
+    """
+
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+            
+
+def close_db(error):
+    """Closes the database again at the end of the request."""
+
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+                    
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+    g.db.close()
+
+
+#app.route
+@app.route('/')
+#def home():
+#    return render_template('home.html')
+def show_entries():
+    db = get_db()
+    cur = db.execute('select title, text from entries order by id desc')
+    entries = cur.fetchall()
+    return render_template('home.html', entries=entries)
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('insert into entries (title, text) values (?, ?)',
+                 [request.form['title'], request.form['text']])
+
+    g.db.commit()
+    flash('New entry war successfully poster')
+    return redirect(url_for('show_entries'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+        error = None
+        if request.method == 'POST':
+            if request.form['username'] != app.config['USERNAME']:
+                error = 'Invalid username'
+            elif request.form['password'] != app.config['PASSWORD']:
+                error = 'Invalid password'
+            else:
+                session['logged_in'] = True
+                flash('You were logged in')
+                return redirect(url_for('show_entries'))
+
+        return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+    
+if __name__ == '__main__':
+    app.run()
